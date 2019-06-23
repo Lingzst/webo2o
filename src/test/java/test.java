44ba21e1ac1af077560d97961bbab8e907ed983c
Jaxier <lingzst@gmail.com>
2019-06-23 21:07:24 +0800

    InterruptedException

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,10 +1,10 @@
 import com.lingzst.webo2o.exceptions.ShopOperationException;
 
 public class test extends base{
-    public void testMethodInit() {
-        int i;
-        System.out.printf("extends");
-    }
+//    public void testMethodInit() {
+//        int i;
+//        System.out.printf("extends");
+//    }
     public static void main(String[] args) {
 
         base a = new test();
