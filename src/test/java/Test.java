fe0036868a4e4822b6078fcb321dd654bf2b2901
Jaxier <lingzst@gmail.com>
2019-06-25 20:25:05 +0800

    InterruptedException

---
+++ src/test/java/Test.java
@@ -0,0 +1,43 @@
+import com.lingzst.webo2o.exceptions.ShopOperationException;
+
+public class Test extends Parent{
+//    public void testMethodInit() {
+//        int i;
+//        System.out.printf("extends");
+//    }
+
+
+    public Test() {
+        normalMethod();
+    }
+
+    public void normalMethod() {
+        System.out.println("test normalMethodCall");
+    }
+
+    @Override
+    public void callParentMethod() {
+        System.out.println("overrided method");
+    }
+
+    public void howToCallParent() {
+        super.callParentMethod();
+    }
+
+    public static void main(String[] args) {
+
+        Parent a =(Parent) new Test();
+        a.callParentMethod();
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+    }
+}
+
+
+class Parent {
+    int a = 1;
+
+    public void callParentMethod() {
+        System.out.println("call  parent method");
+    }
+}
